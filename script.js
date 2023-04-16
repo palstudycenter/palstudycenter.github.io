c144a19893b7ab8ccc0b2d7f5ec37ab3b56840ec
Nayan Pal <nayan.pal@valuelabs.com>
2023-04-16 17:00:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+url = "http://localhost:3000"
+$(document).ready(function () {
+  $("#submit_btn").click(function () {
+    let name = $("#name").val();
+    let phone = $("#phone").val();
+    let st_class = $("#st_class").val();
+    let address = $("#address").val();
+
+    if (name == '' || phone == '' || address == '') {
+      alert("Field can't be empty");
+    } else if (st_class == null) {
+      alert("Please select class");
+    } else {
+      console.log(name, phone, st_class, address);
+      data = { name: name, phone: phone, class: st_class, address: address };
+      $.ajax({
+        url: `${url}/createStudent`,
+        type: 'POST',
+        data: data,
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        success: function (response) {
+          alert(response.result);
+        },
+        error: function (res) {
+          alert("error - " + res.err);
+        }
+    }); 
+    }
+  });
+});
