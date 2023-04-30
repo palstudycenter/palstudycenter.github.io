7b62d7703c1b0851783c4255e86953ee34427f01
Nayan Pal <nayan.pal@valuelabs.com>
2023-04-30 15:26:48 +0530

    update form

--- script.js
+++ script.js
@@ -6,29 +6,57 @@ $(document).ready(function () {
     let phone = $("#phone").val();
     let st_class = $("#st_class").val();
     let address = $("#address").val();
-
+    let st_board = $("#st_board").val();
+    let timing = st_class == "class 10th" ? "9 AM" : (st_class == "class 11th") ? "10 AM" : "8 AM"
     if (name == '' || phone == '' || address == '') {
-      alert("Field can't be empty");
+      Swal.fire(
+        "Field can't be empty",
+        '',
+        'info'
+      )
     } else if (st_class == null) {
-      alert("Please select class");
+      Swal.fire(
+        "Please select class",
+        '',
+        'info'
+      )
+    } else if (st_board == null) {
+      Swal.fire(
+        "Please select board",
+        '',
+        'info'
+      )
     } else {
-      console.log(name, phone, st_class, address);
-      data = { name: name, phone: phone, class: st_class, address: address };
-      $.ajax({
-        url: `${url}/createStudent`,
-        type: 'POST',
-        data: data,
-        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-        success: function (response) {
-          alert(response.result);
-          $("#name").val('');
-          $("#phone").val('');
-          $("#address").val('');
-        },
-        error: function (res) {
-          alert("error - " + res.err);
-        }
-    }); 
+        data = { name: name, phone: phone, class: st_class, address: address, board: st_board };
+        $.ajax({
+          url: `${url}/createStudent`,
+          type: 'POST',
+          data: data,
+          contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+          success: function (response) {
+            $("#name").val('');
+            $("#phone").val('');
+            $("#address").val('');
+            Swal.fire({
+              title: 'Thank you!',
+              text: `Your ${st_class} timing is ${timing}`,
+              icon: 'success',
+              confirmButtonText: 'ok'
+            }).then((result) => {
+              if (result.isConfirmed) {
+                window.location.href = "index.html";
+              }
+            })
+          },
+          error: function (res) {
+            alert("error - " + res.err);
+            Swal.fire({
+              title: 'Error',
+              text: res.err,
+              icon: 'error',
+            })
+          }
+      }); 
     }
   });
 });
