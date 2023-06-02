674a566d6493db3e07a38927b914f0bca0aa72af
Nayan Pal <nayanpalmns@gmail.com>
2023-06-02 14:25:09 +0530

    added students

---
+++ src/testimonial.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  data = [
+    {
+      'img': 'ashish_ojha',
+      'name': 'आशीष ओझा',
+      'work': 'Software Engineer',
+      'country': 'U.S.A'
+    },
+    {
+      'img': 'ragini_soni',
+      'name': 'रागिनी सोनी',
+      'work': 'Software Engineer',
+      'country': 'Australia'
+    } 
+  ]
+
+  
+
+});
